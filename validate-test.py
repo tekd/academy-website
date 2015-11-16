208ead0b0898065e09522b2c6c1dbfb21a81c1a8
Claudio Medonca <claudioccm@Claudios-MacBook-Pro-2.local>
2015-11-16 18:23:08 -0500

    ran build.py to the site folder

--- validate-test.py
+++
@@ -1,8 +0,0 @@
-from json import load as json_load
-from yaml import load as yaml_load
-from jsonschema import validate
-
-schema = json_load(open('data/people_schema.json'))
-PEOPLE = yaml_load(open('data/people.yaml'))
-
-validate(PEOPLE, schema)
