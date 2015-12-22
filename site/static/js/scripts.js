21c156e4f6bca195c28daf157abfbbe5bf2cf742
mocxd <mark.adkins.hastings@gmail.com>
2015-12-22 03:09:13 -0500

    project gallery: adds filters

--- site/static/js/scripts.js
+++ site/static/js/scripts.js
@@ -249,10 +249,78 @@ $('.js-image-gallery-item').click(function() {
   $('#overlay').append(clone).addClass('m-active b-lightbox');
 });
 
+
+// Initialize isotope grid
+var $grid = $('.b-filter').isotope({
+  // options
+  itemSelector: '.b-filter-item',
+  layoutMode: 'fitRows'
+});
+
+// Functionality for filter UI buttons
+$('.b-filter-ui').on( 'click', 'button', function() {
+
+  var filterValue = '';
+
+  // Clicking 'All' filter
+  if ($(this).hasClass('m-clear-filters')) {
+
+    // Deselect all others
+    $('.b-filter-ui').children().each(function() {
+      $(this).removeClass('m-selected');
+    });
+
+    // Can only select, not deselect 'All'
+    $(this).addClass('m-selected');
+
+    filterValue = '*';
+
+  // Clicking any other filter
+  } else {
+
+    $('.b-filter-ui .m-clear-filters').removeClass('m-selected');
+
+    // Swap selection state of button
+    if (!$(this).hasClass('m-selected')) {
+      $(this).addClass('m-selected');
+    } else {
+      $(this).removeClass('m-selected');
+    }
+
+    // Build filter string from all selected filters
+    $('.b-filter-ui').children().each(function() {
+      if ($(this).hasClass('m-selected')) {
+        filterValue += $(this).attr('data-filter');
+      }
+    });
+
+  }
+
+  //console.log(filterValue); //debug
+  selectDefaultFilter();
+  $grid.isotope({ filter: filterValue });
+});
+
+selectDefaultFilter();
+$grid.isotope({ filter: '*' });
+
 }); // Closes Document.ready
 
 
+// If nothing is selected, select 'All' filter
+function selectDefaultFilter() {
+    var noneSelected = true;
 
+    $('.b-filter-ui').children().each(function() {
+      if ($(this).hasClass('m-selected')) {
+        noneSelected = false;
+      }
+    });
+
+    if (noneSelected) {
+      $('.b-filter-ui .m-clear-filters').addClass('m-selected');
+    }
+}
 
 
 function getQuery(param) {
@@ -273,3 +341,5 @@ function getQuery(param) {
 
     return result;
 }
+
+
